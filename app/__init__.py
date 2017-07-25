1b127139bace42df44c2e3c38352c57902620685
Thegaijin <semaganda@outlook.com>
2017-07-26 00:37:09 +0300

    linked the config.py file

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,15 @@ from flask_login import LoginManager
 
 # variable app assigned an instance of class flask
 app = Flask(__name__)
-
+app.config.from_object('config')
 # variable login_manager assigned an instance of class LoginManager
 login_manager = LoginManager()
+
+# For user session management and remember the users’ session
 login_manager.init_app(app)
+login_manager.login_message = "You must be logged in to access this page."
+login_manager.login_view = "login"
+
 
 # import at the end to avoid circular reference
 from app import views
